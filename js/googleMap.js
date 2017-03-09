e3e9e331bcefaab47ca938243fc5dfcbee6186fb
AkoTonissoo <ako.tonissoo@gmail.com>
2017-03-09 02:11:05 +0200

    logini ja mapi failide template

---
+++ js/googleMap.js
@@ -0,0 +1,8 @@
+function myMap() {
+  var myCenter = new google.maps.LatLng(58.379323,26.719372);
+  var mapCanvas = document.getElementById("googleMap");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
