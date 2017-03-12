35ce25f0616166168269dfe2bc052f8d24448d6f
Toomas Veromann <toomasveromann@gmail.com>
2017-03-12 22:57:07 +0200

    fixed html validator issues etc

---
+++ application/language/english/message_lang.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+defined('BASEPATH') OR exit('No direct script access allowed.');
+$lang = array();
+$lang['PAGE_TITLE'] = 'Main page';
+$lang['SITE_NAME'] = 'Jooksupoiss';
+// Menu
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_LOG_IN'] = 'Log In';
+$lang['FOOTER_ABOUT_US'] = 'About Us';
+$lang['FOOTER_FAQ'] = 'FAQ';
+$lang['FOOTER_ABOUT_US'] = 'Contact';
+?>
---
+++ application/language/estonian/message_lang.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+------------------
+Language: Estonian
+------------------
+*/
+defined('BASEPATH') OR exit('No direct script access allowed.');
+$lang = array();
+$lang['PAGE_TITLE'] = 'Pealeht';
+$lang['SITE_NAME'] = 'Jooksupoiss';
+// Menu
+$lang['MENU_HOME'] = 'Kodu';
+$lang['MENU_LOG_IN'] = 'Logi Sisse';
+$lang['FOOTER_ABOUT_US'] = 'Meist';
+$lang['FOOTER_FAQ'] = 'KKK';
+$lang['FOOTER_ABOUT_US'] = 'Kontakt';
+?>
